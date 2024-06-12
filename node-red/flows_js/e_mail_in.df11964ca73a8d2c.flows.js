b8bdcde98fabeffb46bc7d9cea4042fd3bfccf14
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2024-06-12 11:10:27 +0000

    Update mail URLs

--- node-red/flows_js/e_mail_in.df11964ca73a8d2c.flows.js
+++ node-red/flows_js/e_mail_in.df11964ca73a8d2c.flows.js
@@ -5,7 +5,7 @@ const Node = {
   "g": "7927b0d68211546e",
   "name": "",
   "protocol": "IMAP",
-  "server": "imap.randers.dk",
+  "server": "ps.randers.dk",
   "useSSL": false,
   "autotls": "required",
   "port": "143",
