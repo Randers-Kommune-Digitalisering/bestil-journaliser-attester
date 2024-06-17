dd858000e4fd793eb99edc1fca6f03bfebc129f1
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2024-06-17 12:09:48 +0000

    Add catch node and debug to test mail flow

---
+++ node-red/flows_js/debug.fc187aaae4665ec3.flows.js
@@ -0,0 +1,19 @@
+const Node = {
+  "id": "fc187aaae4665ec3",
+  "type": "debug",
+  "z": "3f367bd6f801e4ba",
+  "name": "Test mail ERROR",
+  "active": true,
+  "tosidebar": true,
+  "console": false,
+  "tostatus": false,
+  "complete": "true",
+  "targetType": "full",
+  "statusVal": "",
+  "statusType": "auto",
+  "x": 730,
+  "y": 200,
+  "wires": []
+}
+
+module.exports = Node;
