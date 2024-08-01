590a1e7a4f2958bef77d56463f2cad81612cc0d4
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2024-08-01 12:14:40 +0000

    Add test to force journalize

---
+++ node-red/flows_js/change.0d27558852413c06.flows.js
@@ -0,0 +1,30 @@
+const Node = {
+  "id": "0d27558852413c06",
+  "type": "change",
+  "z": "90c6b0b502e346fa",
+  "g": "70f0dd8ceb56e8a4",
+  "name": "TEST NODE",
+  "rules": [
+    {
+      "t": "set",
+      "p": "payload",
+      "pt": "msg",
+      "to": "payload[0]",
+      "tot": "msg"
+    }
+  ],
+  "action": "",
+  "property": "",
+  "from": "",
+  "to": "",
+  "reg": false,
+  "x": 650,
+  "y": 1920,
+  "wires": [
+    [
+      "b8d26dd87dd4156e"
+    ]
+  ]
+}
+
+module.exports = Node;
